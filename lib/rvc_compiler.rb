444daa8b064e499d630d3e1dfe208ceff0178fd3
Carlos Quinones <cquinones100@gmail.com>
2019-03-24 21:00:41 -0400

    initial commit

---
+++ lib/rvc_compiler.rb
@@ -0,0 +1,6 @@
+require "rvc_compiler/version"
+
+module RvcCompiler
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rvc_compiler_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe RvcCompiler do
+  it "has a version number" do
+    expect(RvcCompiler::VERSION).not_to be nil
+  end
+end
